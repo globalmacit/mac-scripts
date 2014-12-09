51be518df4222e353d9643d49d3749731a69b182
Tobias Morrison <t@cmndhlp.com>
2014-12-09 09:59:35 -0500

    Added variable for User

--- remote-management.sh
+++ remote-management.sh
@@ -5,14 +5,17 @@
 #	version: 0.1
 #	created: 07 Nov 2014
 #	author:  Tobias Morrison 
+#	credit: http://support.apple.com/en-us/ht2370
 #
 #	Enables ARD, grants access and all privileges to the user 'gadmin'
 #	Restarts the ARD agent and the ARD menu extra
 #	Turns on ssh
 
+# Set your user
+User="gadmin"
 
-#	Turn on ARD and restrict to 'gadmin' 
-/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users gadmin -privs -all -restart -agent
+#	Turn on ARD and restrict to the above user 
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users $User -privs -all -restart -agent
 
 #	Turn on remote login
 /usr/sbin/systemsetup -f -setremotelogin on
