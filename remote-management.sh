613a482248c7d6e2a904fe55807b9868f876f009
Tobias Morrison <t@cmndhlp.com>
2015-02-17 12:05:57 -0500

    Improved kickstart commands for Yosemite.

--- remote-management.sh
+++ remote-management.sh
@@ -2,20 +2,25 @@
 
 
 #	remote-management.sh
-#	version: 0.1
+#	version: 0.2
 #	created: 07 Nov 2014
 #	author:  Tobias Morrison 
 #	credit: http://support.apple.com/en-us/ht2370
 #
-#	Enables ARD, grants access and all privileges to the user 'gadmin'
+#	modified: 17 Feb 2015
+#	made Remote Management restart its own command as it is more reliable on Yosemite.
+#	removed restriction to a particular user to accommodate new remote tools.
+#
+#	Enables ARD, grants access and all privileges to all users
 #	Restarts the ARD agent and the ARD menu extra
 #	Turns on ssh
 
-# Set your user
-User="gadmin"
 
 #	Turn on ARD and restrict to the above user 
-/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users $User -privs -all -restart -agent
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -activate -access -on -privs -all -allowAccessFor -allUsers
+
+#	Restart Remote Management to accept the changes
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent
 
 #	Turn on remote login
 /usr/sbin/systemsetup -f -setremotelogin on
