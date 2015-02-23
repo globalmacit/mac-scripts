4d45fb93ea5cff8887d22e958e16047a9eccb1e7
Tobias Morrison <t@cmndhlp.com>
2015-02-23 00:10:21 -0500

    Fixed capture of Full Name.
    Found solution using egrep to pull name from finger results.

--- rename-computer.sh
+++ rename-computer.sh
@@ -10,34 +10,36 @@
 
 
 #	Provide the company abbreviated name.
-COMPANY="WidgetCo"
+COMPANY="GlobalMac IT"
 
 ### Let the robot do the work.
+#	Get the currently logged in user name.
+NAME=`finger $USER | egrep -o 'Name: [a-zA-Z0-9 ]{1,}' | cut -d ':' -f 2 | xargs echo`
+
 #	Grab some info from the system.
 SYSTEM=`system_profiler SPHardwareDataType | grep "Model Identifier:" | awk '{print $3;}' | sed 's/[0-9][,]*//g' | sed 's/Macmini/MM/g' | sed 's/MacBookPro/MBP/g' | sed 's/MacBookAir/MBA/g' | sed 's/MacBook/MB/g'`
 
-
-#	Get the currently logged in user name.
-NAME=`finger `whoami` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //'`
-
 #	A simple function to adjust the case of the variable outputs.
 makelower() {
-echo $1 | tr '[:upper:]' '[:lower:]' | sed 's/ //g'
+echo $1$2 | tr '[:upper:]' '[:lower:]' | sed 's/ //g'
 }
 
 L_NAME=`makelower $NAME` 
 L_COMPANY=`makelower $COMPANY`
 L_SYSTEM=`makelower $SYSTEM`
 
-/usr/sbin/scutil --set ComputerName $NAME-$SYSTEM-$COMPANY
-/usr/sbin/scutil --set LocalHostName $L_NAME-$L_SYSTEM-$L_COMPANY
-/usr/sbin/scutil --set HostName $L_NAME-$L_SYSTEM-$L_COMPANY.local
+sudo /usr/sbin/scutil --set ComputerName "$NAME $SYSTEM $COMPANY"
+sudo /usr/sbin/scutil --set LocalHostName $L_NAME-$L_SYSTEM-$L_COMPANY
+sudo /usr/sbin/scutil --set HostName $L_NAME-$L_SYSTEM-$L_COMPANY.local
 
 #	Tell me the results.
 status=$?
 
 if [ $status == 0 ]; then
 	echo "Computer identity was changed successfully."
+	echo "$NAME $SYSTEM $COMPANY"
+	echo "$L_NAME-$L_SYSTEM-$L_COMPANY"
+	echo "$L_NAME-$L_SYSTEM-$L_COMPANY.local"
 elif [ $status != 0 ]; then
 	echo "An error was encountered while attempting to change the computer identity. /usr/sbin/scutil exited $status."
 fi
