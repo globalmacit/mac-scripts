d776b6cd6e6689e44bdb5d34a7389db14ad1f185
Tobias Morrison <t@cmndhlp.com>
2015-05-28 16:02:30 -0400

    change poorly chosen variable name

--- extract-prev-recpts.sh
+++ extract-prev-recpts.sh
@@ -10,31 +10,31 @@
 
 
 #  Define our local user
-USER="$(whoami)"
+MYUSER="$(whoami)"
 
 #  Give the file a logical name
-FILE="$USER-contacts.csv"
+FILE="$MYUSER-contacts.csv"
 
-#	Current directory containing the script.
-#+	This allows us to run the script and save the
-#+	results to the removable media. 
-#+	Source: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+#  Current directory containing the script.
+#  This allows us to run the script and save the
+#  results to the removable media. 
+#  Source: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #  File output path
 FILEOUTPUT="$DIR/$FILE"
 
 # Okay, robots. Do your thing.
-/usr/bin/sqlite3 /Users/"$USER"/Library/Application\ Support/AddressBook/MailRecents-v4.abcdmr <<!
+/usr/bin/sqlite3 /Users/"$MYUSER"/Library/Application\ Support/AddressBook/MailRecents-v4.abcdmr <<!
 .headers on
 .mode csv
 .output "$FILEOUTPUT"
 select ZLASTNAME, ZFIRSTNAME, ZEMAIL from ZABCDMAILRECENT;
 !
 
+sleep 2
+
 echo ""$FILEOUTPUT" saved."
 open "$DIR"
-
-sleep 2
 
 exit 0
