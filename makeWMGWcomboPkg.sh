961e3374bb3cab003b1f0aa1af4c339852172ddd
Tobias Morrison <tobias@globalmacit.com>
2018-02-28 14:30:14 -0500

    add BlueSky and account installers

--- makeWMGWcomboPkg.sh
+++ makeWMGWcomboPkg.sh
@@ -6,6 +6,8 @@
 # EDIT THESE!!!!!
 wmServer="globalmacit" #This is the subdomain for your watchman server. Eg. if your watchman server is acme.monitoringclient.com, enter "acme"
 gwInstaller="$HOME/bin/onboarding/GlobalMacIT Maintenance.pkg" #This is the path to where you keep the GW installer on your machine, eg. /Users/user/Desktop/GruntworkInstaller.pkg
+bsInstaller="$HOME/bin/onboarding/GlobalMacIT Remote.pkg" #This is the path to where you keep the BS installer on your machine, eg. /Users/user/Desktop/BlueSkyInstaller.pkg
+guInstaller="$HOME/bin/onboarding/create_gadmin-2.1.1.pkg" #This is the path to where you keep the admin installer on your machine, eg. /Users/user/Desktop/admin.pkg
 
 wmClient="!! CLIENT NAME !!" #the WM client group name
 outputPath="/tmp/unsignedInstaller-$wmClient.pkg" #full output path for the finished pkg
@@ -48,25 +50,26 @@ if [ $? -ne 0 ]; then
 fi
 
 ## Create the metapkg
-productbuild --package "/tmp/1wmInstall.pkg" --package "$gwInstaller" "$outputPath"
+productbuild --package "/tmp/1wmInstall.pkg" --package "$gwInstaller" --package "$bsInstaller" --package "$guInstaller" "$outputPath"
 if [ $? -ne 0 ]; then
 	echo "Problem building the metapkg package."
 	exit 5
 else
 	productsign --sign "!! DEVELOPER INSTALLER ID !!" "$outputPath" "$signedOutputPath"
-	zip "$zippedOutput" "$signedOutputPath"
+	sleep 5 # productsign needs some time to run
+	cd /tmp # zip is dumb and only likes to work in the current directory
+	zip "$zippedOutput" "ManagementInstaller-$wmClient.pkg"
 	if [ $? -ne 0 ]; then
 		echo "Problem signing the package."
 		exit 6
 	else
-		sleep 2
-		echo "Success! Your combo installer pkg is done: "/tmp/$zippedOutput"
+		echo "Success! Your combo installer pkg is done: /tmp/$zippedOutput"
 		# Show me the money
-		/usr/bin/open "/tmp"
+		open "/tmp"
 		# Tidy up
-		/bin/rm /tmp/1wmInstall.pkg
-		/bin/rm -rf /tmp/pkgScripts
-		/bin/rm "$outputPath"
+		rm /tmp/1wmInstall.pkg
+		rm -rf /tmp/pkgScripts
+		rm "$outputPath"
 	fi
 fi
 
